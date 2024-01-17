bddab04db04f40867c2adb9624b1f7b05336ae8a
Tiffany <Tiffany@TIFFANYZANG4BB3>
2024-01-17 16:43:59 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Mission2;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        //new RollDice class
+        RollDice dr = new RollDice();
+
+        //create variable for player input
+        string RollInput = "";
+
+        //create variable for number of rolls
+        int RollNum = 0;
+
+        //Write welcome message and ask for user input
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+
+        //user input
+        RollInput = System.Console.ReadLine();
+
+        //change user input to integer
+        RollNum = int.Parse(RollInput);
+
+        int[] array = dr.DiceRollResults(RollNum);
+
+        //Write the result message with total number of rolls 
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS\n" +
+        "Each \"*\" represents 1% of the total number of rolls.\n" +
+        "Total Number of Rolls = " + RollNum + "\n");
+
+        //loop to create the histogram
+        for (int rollResult = 2; rollResult < 13; rollResult++) 
+        {
+            //print each of the possible roll values (2-12)
+            System.Console.Write(rollResult + ": ");
+
+            //calculate the percentage
+            int rollpercent = (array[rollResult] * 100 / RollNum);
+
+            //print the number of stars based off of the percentage 
+            for (int stars = 0; stars < rollpercent; stars++)
+            {
+                System.Console.Write("*");
+            }
+
+            System.Console.WriteLine();
+        }
+
+        //Write ending message to end the simulator 
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+
+
+    }
+}
+
---
+++ RollDice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class RollDice
+    {
+        public int[] DiceRollResults(int RollNum)
+        {
+            //create an array
+            int[] arr = new int[13];
+
+            //loop through to get a dice throw for each roll
+            for (int roll = 0; roll < RollNum; roll++)
+            {
+                //generate a random number for "dice 1"
+                Random rnd = new Random();
+                //assign it to a variable for "dice 1"
+                int roll1 = rnd.Next(1, 7);
+
+                //generate a new random number for "dice 2"
+                Random rnd2 = new Random();
+                //assign it to a variable for "dice 2"
+                int roll2 = rnd2.Next(1, 7);
+
+                //total dice roll and access that specific array and add 1 (for rolled once)
+                arr[roll1 + roll2]++;
+                
+            }
+
+            //return array with results
+            return arr;
+
+        }
+
+    }
+}
